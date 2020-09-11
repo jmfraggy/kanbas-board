2fcbbd4a62501ead33dd951ec9b9252c0a108c3f
jmfraggy <jmfofragoso@gmail.com>
2020-09-11 16:17:34 -0500

    Production Static Serve with Express

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const express = require('express');
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
 
+// Serve static assets in production 
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build',
+        'index.html')));
+}
+
+const port = process.env.PORT || 5000;
 server.listen(port);
